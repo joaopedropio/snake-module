7b1ad7cc9e0d0daf1b2bcc98fe2b5300c983aa53
Joao Pedro Pio <joao-pio@hotmail.com>
2025-08-18 16:48:05 -0300

    Add flash

--- boards/shields/snake_adapter/widgets/layer_switch.c
+++ boards/shields/snake_adapter/widgets/layer_switch.c
@@ -29,6 +29,8 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include "battery_status.h"
 #include "helpers/display.h"
 #include "theme.h"
+//#include "snake_image.h"
+#include "logo.h"
 
 static uint8_t *buf_frame;
 
@@ -48,16 +50,20 @@ struct layer_status_state {
 };
 
 void print_frames() {
+    // logo frame
+    print_rectangle(buf_frame, 0, 238, 0, 108, get_frame_color(), 2);
+    print_rectangle(buf_frame, 2, 236, 2, 106, get_frame_color_1(), 2);
+
     // board frame
-    print_rectangle(buf_frame, 0, 237, 0, 237, get_frame_color(), 2);
+    print_rectangle(buf_frame, 0, 237, 110, 237, get_frame_color(), 2);
 
     // status frames
-    print_rectangle(buf_frame, 2, 119, 2, 158, get_frame_color(), 2);
-    print_rectangle(buf_frame, 4, 117, 4, 156, get_frame_color_1(), 2);
+    print_rectangle(buf_frame, 2, 119, 112, 158, get_frame_color(), 2);
+    print_rectangle(buf_frame, 4, 117, 114, 156, get_frame_color_1(), 2);
 
     // theme frames
-    print_rectangle(buf_frame, 121, 236, 2, 158, get_frame_color(), 2);
-    print_rectangle(buf_frame, 123, 234, 4, 156, get_frame_color_1(), 2);
+    print_rectangle(buf_frame, 121, 236, 112, 158, get_frame_color(), 2);
+    print_rectangle(buf_frame, 123, 234, 114, 156, get_frame_color_1(), 2);
 
     // battery frames 
     print_rectangle(buf_frame, 2, 119, 160, 236, get_frame_color(), 2);
@@ -68,7 +74,8 @@ void print_frames() {
 }
 
 void print_menu() {
-    clear_area();
+    clear_screen();
+    start_animation();
     print_frames();
     start_battery_status();
     start_output_status();
@@ -81,6 +88,7 @@ void toggle_menu() {
     if (menu_on) {
         stop_output_status();
         stop_battery_status();
+        stop_animation();
         restart_snake();
         menu_on = false;
     } else {
@@ -94,6 +102,7 @@ void change_theme() {
     set_next_theme();
     if (menu_on) {
         print_menu();
+        apply_theme_snake();
     } else {
         stop_snake();
         apply_theme_snake();
@@ -124,30 +133,46 @@ void set_layer_symbol() {
     dongle_lock = false;
 }
 
-static void layer_status_update_cb(struct layer_status_state state) {
+// static void layer_status_update_cb(struct layer_status_state state) {
+//     ls_state = state;
+//     if (layer_switch_initialized) {
+//         set_layer_symbol();
+//     }
+// }
+
+// static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
+//     uint8_t index = zmk_keymap_highest_layer_active();
+//     return (struct layer_status_state) {
+//         .index = index,
+//         .label = zmk_keymap_layer_name(index)
+//     };
+// }
+
+// ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
+//                             layer_status_get_state)
+
+// ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
+
+static int tap_dance_position_state_changed_listener(const zmk_event_t *eh);
+
+ZMK_LISTENER(behavior_dongle_action, dongle_action_position_state_changed_listener);
+ZMK_SUBSCRIPTION(behavior_dongle_action, zmk_position_state_changed);
+
+
+static int tap_dance_position_state_changed_listener(const zmk_event_t *eh) {
     ls_state = state;
     if (layer_switch_initialized) {
         set_layer_symbol();
     }
+    return ZMK_BEHAVIOR_OPAQUE;
 }
-
-static struct layer_status_state layer_status_get_state(const zmk_event_t *eh) {
-    uint8_t index = zmk_keymap_highest_layer_active();
-    return (struct layer_status_state) {
-        .index = index,
-        .label = zmk_keymap_layer_name(index)
-    };
-}
-
-ZMK_DISPLAY_WIDGET_LISTENER(widget_layer_status, struct layer_status_state, layer_status_update_cb,
-                            layer_status_get_state)
-
-ZMK_SUBSCRIPTION(widget_layer_status, zmk_layer_state_changed);
 
 void zmk_widget_layer_switch_init() {
-	widget_layer_status_init();
+	//widget_layer_status_init();
 
     buf_frame = (uint8_t*)k_malloc(320 * 2 * sizeof(uint8_t));
+
+    //snake_image_buf = k_malloc(snake_image_width * 2 * sizeof(uint16_t));
 }
 
 void start_layer_switch() {
